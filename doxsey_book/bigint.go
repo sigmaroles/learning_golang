cf2ddfc34145895e56547bd91a13a0185507f44e
sigmaroles <sohhom@gmail.com>
2019-10-17 11:46:24 +0530

    not working - big int add

--- doxsey_book/bigint.go
+++ doxsey_book/bigint.go
@@ -6,6 +6,12 @@ import (
 )
 
 func main() {
-	aa := big.Int(2)
+	aa := big.NewInt(2)
 	fmt.Println("The int is ", aa)
+	fmt.Println("The sum is ", testf(a))
+}
+
+func testf(a int) big.Int {
+	a1 := big.NewInt(a)
+	return big.NewInt(a1 + a1*3)
 }
