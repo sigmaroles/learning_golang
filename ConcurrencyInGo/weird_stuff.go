5abbc05705633b338707b819f0013c814dc573bb
sigmaroles <sohhom@gmail.com>
2019-11-25 20:35:00 +0530

    iterated race tests

--- ConcurrencyInGo/weird_stuff.go
+++ ConcurrencyInGo/weird_stuff.go
@@ -2,23 +2,31 @@ package main
 
 import "fmt"
 
-func main() {
-	data := 0
+func raceTest() {
+	data := 100
 	iter := 0
-	for data < 10 {
+	for data > 80 {
 
 		go func() {
-			fmt.Println("Plus func; got data = ", data)
+			//fmt.Println("Plus func; got data = ", data)
 			data++
 		}()
 
 		go func() {
-			fmt.Println("Minus func; got data = ", data)
+			//fmt.Println("Minus func; got data = ", data)
 			data--
 		}()
 
 		iter++
-		fmt.Println("iter = ", iter, " data : ", data)
+		//fmt.Println("iter = ", iter, " data : ", data)
 	}
-	fmt.Println("\nFinal value is ", data)
+	fmt.Println("\tFinal data = ", data, " ; No. of iterations = ", iter)
+}
+
+func main() {
+	for i := 0; i < 20; i++ {
+		fmt.Print("Race test iter ", i)
+		raceTest()
+	}
+
 }
