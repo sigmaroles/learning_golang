1e1b78e69199a962c60fca86fb2c001f44cefe7a
sigmaroles <sohhom@gmail.com>
2022-06-23 08:22:04 +0530

    n1 from 2022

---
+++ database_etc/redis1.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/go-redis/redis"
+)
+
+func main() {
+	fmt.Println("Go Redis Tutorial")
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
+
+	data := client.Get("data")
+	fmt.Println(data.String())
+
+}
